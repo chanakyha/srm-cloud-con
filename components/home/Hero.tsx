ad65e96ed3e04ea3445bf57db39d45563eeaf3bd
chanakyha <vetrichanakyha2003@gmail.com>
2024-01-29 00:52:41 +0530

    Replaced cloud con

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="relative h-fix">
       <div className="relative flex flex-col items-center justify-center h-fix gap-2 w-screen p-4">
         <h1 className="text-center text-3xl font-bold md:text-5xl lg:text-7xl ">
-          AI ZYPHER
+          Cloud Con'24
         </h1>
         <p className="text-center text-xl md:text-2xl font-medium">
           Admin Dashboard
